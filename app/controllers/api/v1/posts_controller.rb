2cd805776ac7f25b1e89022180f2165330e962cc
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2018-09-04 09:31:18 -0400

    Added rack cors to gemfile and enabled cross origin resource sharing

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,29 +1,30 @@
 # frozen_string_literal: true
 
 # posts_controller.rb
+module Api::V1
+  class PostsController < ApplicationController
+    def index
+      respond_with Post.all
+    end
 
-class Api::V1::PostsController < Api::V1::BaseController
-  def index
-    respond_with Post.all
-  end
+    def create
+      respond_with :api, :v1, Post.create(post_params)
+    end
 
-  def create
-    respond_with :api, :v1, Post.create(post_params)
-  end
+    def destroy
+      respond_with Post.destroy(params[:id])
+    end
 
-  def destroy
-    respond_with Post.destroy(params[:id])
-  end
+    def update
+      post = Post.find(params["id"])
+      post.update_attributes(post_params)
+      respond_with post, json: post
+    end
 
-  def update
-    post = Post.find(params["id"])
-    post.update_attributes(post_params)
-    respond_with post, json: post
-  end
+    private
 
-  private
-
-  def post_params
-    params.require(:post).permit(:id, :name, :description)
+    def post_params
+      params.require(:post).permit(:id, :title, :note)
+    end
   end
 end
