3a5aa017af9b1fa56d49a992194c5d5710598a52
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2018-09-05 22:50:47 -0400

    made it so that its not limited to localhost:3000

--- config/application.rb
+++ config/application.rb
@@ -34,8 +34,8 @@ module RailsApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
   end
